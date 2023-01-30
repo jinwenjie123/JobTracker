181b71952ee3e89bb965af2dea94d95a416eb056
jinwenjie123 <jinwenjie00@gmail.com>
2023-01-29 22:14:37 -0800

    implemented addTab

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 let myJobs = [];
 const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 
-inputBtn.addEventListener("click", function(){
-    myJobs.push(inputEl.value);
-    renderJobs();
-    inputEl.value = "";
+const jobsFromLocalStorage = JSON.parse(localStorage.getItem("myJobs"));
+
+if(jobsFromLocalStorage){
+    myJobs = jobsFromLocalStorage;
+    render(myJobs);
+}
+
+tabBtn.addEventListener("click", function(){
+    //use the google chrome api to fetch the current tab 
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myJobs.push(tabs[0].url);
+        localStorage.setItem("myJobs", JSON.stringify(myJobs));
+        render(myJobs);
+    });
 });
 
-function renderJobs(){
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear();
+    myJobs = [];
+    render(myJobs);
+});
+
+inputBtn.addEventListener("click", function(){
+    myJobs.push(inputEl.value);
+    inputEl.value = "";
+
+    localStorage.setItem("myJobs", JSON.stringify(myJobs));
+    render(myJobs);
+});
+
+function render(jobs){
     let listItem = "";
-    for(let i = 0; i < myJobs.length; i++){
-        listItem += `<li><a href="${myJobs[i]}" target="_blank">${myJobs[i]}</li>`;
+    for(let i = 0; i < jobs.length; i++){
+        listItem += `<li><a href="${jobs[i]}" target="_blank">${jobs[i]}</li>`;
     }
     ulEl.innerHTML = listItem;
 }
